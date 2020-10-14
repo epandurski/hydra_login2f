41b9c68d252c14e027a2c6d490b5c0fd65f72511
Evgeni Pandurski <epandurski@gmail.com>
2020-10-14 14:58:25 +0300

    Update Pipfile.lock, Use hydra 1.8.5

--- hydra_login2f/hydra.py
+++ hydra_login2f/hydra.py
@@ -12,10 +12,10 @@ def invalidate_credentials(user_id):
     UserLoginsHistory(user_id).clear()
     subject = quote_plus(get_subject(user_id))
     timeout = current_app.config['HYDRA_REQUEST_TIMEOUT_SECONDS']
-    hydra_consents_base_url = urljoin(current_app.config['HYDRA_ADMIN_URL'], '/oauth2/auth/sessions/consent/')
-    hydra_logins_base_url = urljoin(current_app.config['HYDRA_ADMIN_URL'], '/oauth2/auth/sessions/login/')
-    requests.delete(hydra_consents_base_url + subject, timeout=timeout)
-    requests.delete(hydra_logins_base_url + subject, timeout=timeout)
+    hydra_consents_base_url = urljoin(current_app.config['HYDRA_ADMIN_URL'], '/oauth2/auth/sessions/consent')
+    hydra_logins_base_url = urljoin(current_app.config['HYDRA_ADMIN_URL'], '/oauth2/auth/sessions/login')
+    requests.delete(f'{hydra_consents_base_url}?subject={subject}', timeout=timeout)
+    requests.delete(f'{hydra_logins_base_url}?subject={subject}', timeout=timeout)
 
 
 class LoginRequest:
@@ -25,10 +25,12 @@ class LoginRequest:
         """Too many login attempts."""
 
     def __init__(self, challenge_id):
-        self.challenge_id = challenge_id
+        self.challenge_id = quote_plus(challenge_id)
         self.timeout = current_app.config['HYDRA_REQUEST_TIMEOUT_SECONDS']
-        base_url = urljoin(current_app.config['HYDRA_ADMIN_URL'], '/oauth2/auth/requests/login/')
-        self.request_url = base_url + challenge_id
+        base_url = urljoin(current_app.config['HYDRA_ADMIN_URL'], '/oauth2/auth/requests/')
+        self.fetch_url = urljoin(base_url, 'login')
+        self.accept_url = urljoin(base_url, 'login/accept')
+        self.reject_url = urljoin(base_url, 'login/reject')
 
     def register_successful_login(self, subject):
         key = self.LOGIN_COUNT_SUBJECT_PREFIX + subject
@@ -40,7 +42,7 @@ class LoginRequest:
     def fetch(self):
         """Return the subject if already logged, `None` otherwise."""
 
-        r = requests.get(self.request_url, timeout=self.timeout)
+        r = requests.get(f'{self.fetch_url}?login_challenge={self.challenge_id}', timeout=self.timeout)
         r.raise_for_status()
         fetched_data = r.json()
         return fetched_data['subject'] if fetched_data['skip'] else None
@@ -52,7 +54,7 @@ class LoginRequest:
             self.register_successful_login(subject)
         except self.TooManyLogins:
             return self.reject()
-        r = requests.put(self.request_url + '/accept', timeout=self.timeout, json={
+        r = requests.put(f'{self.accept_url}?login_challenge={self.challenge_id}', timeout=self.timeout, json={
             'subject': subject,
             'remember': remember,
             'remember_for': remember_for,
@@ -63,7 +65,7 @@ class LoginRequest:
     def reject(self):
         """Reject the request, return an URL to redirect to."""
 
-        r = requests.put(self.request_url + '/reject', timeout=self.timeout, json={
+        r = requests.put(f'{self.reject_url}?login_challenge={self.challenge_id}', timeout=self.timeout, json={
             'error': 'too_many_logins',
             'error_description': 'Too many login attempts have been made in a given period of time.',
             'error_hint': 'Try again later.',
@@ -74,15 +76,16 @@ class LoginRequest:
 
 class ConsentRequest:
     def __init__(self, challenge_id):
-        self.challenge_id = challenge_id
+        self.challenge_id = quote_plus(challenge_id)
         self.timeout = current_app.config['HYDRA_REQUEST_TIMEOUT_SECONDS']
-        base_url = urljoin(current_app.config['HYDRA_ADMIN_URL'], '/oauth2/auth/requests/consent/')
-        self.request_url = base_url + challenge_id
+        base_url = urljoin(current_app.config['HYDRA_ADMIN_URL'], '/oauth2/auth/requests/')
+        self.fetch_url = urljoin(base_url, 'consent')
+        self.accept_url = urljoin(base_url, 'consent/accept')
 
     def fetch(self):
         """Return the list of requested scopes, or an empty list if no consent is required."""
 
-        r = requests.get(self.request_url, timeout=self.timeout)
+        r = requests.get(f'{self.fetch_url}?consent_challenge={self.challenge_id}', timeout=self.timeout)
         r.raise_for_status()
         fetched_data = r.json()
         return [] if fetched_data['skip'] else fetched_data['requested_scope']
@@ -90,7 +93,7 @@ class ConsentRequest:
     def accept(self, grant_scope, remember=False, remember_for=0):
         """Approve the request, return an URL to redirect to."""
 
-        r = requests.put(self.request_url + '/accept', timeout=self.timeout, json={
+        r = requests.put(f'{self.accept_url}?consent_challenge={self.challenge_id}', timeout=self.timeout, json={
             'grant_scope': grant_scope,
             'remember': remember,
             'remember_for': remember_for,
